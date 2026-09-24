c931069c5a3c34c1571d3a23225abe887acdd4b6
Tiziano Trimboli <ttrimboli@frba.utn.edu.ar>
2026-09-24 20:05:04 -0300

    arreglando los octales del a1

--- automata1.c
+++ automata1.c
@@ -1,66 +1,54 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int columna(int e, char c)
 {
-    if (c == '@') return 8;
+    if (c == '@') {
+        if (e == 1 || e == 3 || e == 4 || e == 6) return 8;
+    }
 
     if (e == 0) {
         if (c == '0') return 0;
-        if (c >= '1' && c <= '7') return 1;
+        if (c >= '1' && c <= '9') return 3;
         if (c == '+' || c == '-') return 2;
-        if (c >= '8' && c <= '9') return 3;
     }
-
-    if (e == 1) {
-        if (c == '0') return 0;
-        if (c >= '1' && c <= '7') return 1;
-        if (c >= '8' && c <= '9') return 3;
-    }
-
-    if (e == 2) {
-        if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F')) return 7;
-    }
-
-    if (e == 3) {
+    else if (e == 1) {
+        if (c == '0' || (c >= '1' && c <= '7')) return 5;
         if (c == 'x' || c == 'X') return 6;
     }
-
-    if (e == 4) {
-        if (c >= '0' && c <= '7') return 5;
-        if (c == '8' || c == '9') return 4;
+    else if (e == 2) {
+        if (c >= '1' && c <= '9') return 3;
     }
-
-    if (e == 5) {
+    else if (e == 3) {
         if (c >= '0' && c <= '9') return 4;
     }
-
-    if (e == 6) {
+    else if (e == 4) {
+        if (c == '0' || (c >= '1' && c <= '7')) return 5;
     }
-
-    if (e == 7) {
+    else if (e == 5 || e == 6) {
         if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F')) return 7;
     }
 
     return -1;
 }
 
+
 int esPalabraLeng(const char *cadena)
 {
-    static int tt[8][9] = {
-        {  3,   4,   1,   5,  -1,  -1,  -1,  -1, -1 },
-        {  6,   4,  -1,   5,  -1,  -1,  -1,  -1, -1 },
-        { -1,  -1,  -1,  -1,  -1,  -1,  -1,   7, -1 },
-        { -1,  -1,  -1,  -1,  -1,  -1,   2,  -1,  0 },
-        { -1,  -1,  -1,  -1,   5,   4,  -1,  -1,  0 },
-        { -1,  -1,  -1,  -1,   5,  -1,  -1,  -1,  0 },
-        { -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  0 },
-        { -1,  -1,  -1,  -1,  -1,  -1,  -1,   7,  0 }
+    static const int tt[7][9] = {
+       {  1, -1,  2,  3, -1, -1, -1, -1, -1 },
+       { -1, -1, -1, -1, -1,  4,  5, -1,  0 },
+       { -1, -1, -1,  3, -1, -1, -1, -1, -1 },
+       { -1, -1, -1, -1,  3, -1, -1, -1,  0 },
+       { -1, -1, -1, -1, -1,  4, -1, -1,  0 },
+       { -1, -1, -1, -1, -1, -1, -1,  6, -1 },
+       { -1, -1, -1, -1, -1, -1, -1,  6,  0 }
     };
 
     int e = 0;
     int i = 0;
-    int c = cadena[0];
+    char c = cadena[0];
 
     while (c != '\0' && e != -1) {
         int col = columna(e, c);
@@ -73,7 +61,8 @@ int esPalabraLeng(const char *cadena)
         c = cadena[i];
     }
 
-    return e == 3 || e == 4 || e == 5 || e == 6 || e == 7;
+    
+    return (e == 1 || e == 3 || e == 4 || e == 6);
 }
 
 int verifica(const char *s)
@@ -84,28 +73,78 @@ int verifica(const char *s)
     for (unsigned i = 0; s[i] != '\0'; i++) {
         unsigned char c = (unsigned char)s[i];
 
-if (!(isdigit(c) || c == '+' || c == '-' || c == '@' ||
-c == 'x' || c == 'X' ||
-(c >= 'a' && c <= 'f') ||
-(c >= 'A' && c <= 'F')))
-return 0;
+        if (!(isdigit(c) || c == '+' || c == '-' || c == '@' ||
+              c == 'x' || c == 'X' ||
+              (c >= 'a' && c <= 'f') ||
+              (c >= 'A' && c <= 'F')))
+            return 0;
+    }
+
+    return 1;
 }
 
-return 1;
+/* Determina el tipo de constante individual */
+const char *clasificarSegmento(const char *s)
+{
+    const char *p = s;
+
+    if (*p == '+' || *p == '-')
+        p++;
+
+    if (p[0] == '0' && (p[1] == 'x' || p[1] == 'X'))
+        return "Hexadecimal";
+
+    if (p[0] == '0' && p[1] != '\0')
+        return "Octal";
+
+    return "Decimal";
+}
+
+
+void clasificarCadenaCompleta(const char *cadena)
+{
+    char copia[256];
+    strncpy(copia, cadena, sizeof(copia) - 1);
+    copia[sizeof(copia) - 1] = '\0';
+
+    int hex = 0, dec = 0, oct = 0;
+    char *subcadena = strtok(copia, "@");
+
+    printf("\n--- Desglose de Constantes ---\n");
+    while (subcadena != NULL) {
+        const char *tipo = clasificarSegmento(subcadena);
+        printf("  Segmento \"%s\" -> %s\n", subcadena, tipo);
+
+        if (strcmp(tipo, "Hexadecimal") == 0)
+            hex++;
+        else if (strcmp(tipo, "Octal") == 0)
+            oct++;
+        else
+            dec++;
+
+        subcadena = strtok(NULL, "@");
+    }
+
+    printf("\n--- Resumen ---\n");
+    printf("Decimales  : %d\n", dec);
+    printf("Octales    : %d\n", oct);
+    printf("Hexadecim. : %d\n", hex);
 }
 
 int main(void)
 {
-char palabra[256];
+    char palabra[256];
 
-printf("Introduce una cadena: ");
-if (scanf("%255s", palabra) != 1)
-return 1;
+    printf("Introduce la cadena de constantes (separadas por @): ");
+    if (scanf("%255s", palabra) != 1)
+        return 1;
 
-if (verifica(palabra) && esPalabraLeng(palabra))
-printf("Cadena valida\n");
-else
-printf("Cadena invalida\n");
+    if (verifica(palabra) && esPalabraLeng(palabra)) {
+        printf("\nCadena global VALIDA\n");
+        clasificarCadenaCompleta(palabra);
+    } else {
+        printf("\nCadena INVALIDA segun el automata.\n");
+    }
 
     return 0;
 }
